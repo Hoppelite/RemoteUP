8565c3ce323a3a69caa67b339bff20f3d03aa3cb
hoppelite <hoppelite@gmail.com>
2018-04-09 23:52:56 +1200

    Add Application to source

---
+++ FTPSync/FTPSync/FTP.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FTPSync
+{
+    /// <summary>
+    /// Controls basic FTP commands
+    /// </summary>
+    public class FTP
+    {
+        private string host;
+        private string username;
+        private string password;
+        private string port;
+        private string remotePath;
+
+        /// <summary>
+        /// Create FTP object with default parameters
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="port"></param>
+        /// <param name="remotePath"></param>
+        public FTP(string host = "localhost", string username = "anonymous", string password = "anonymous", string port = "21", string remotePath = "/")
+        {
+            this.host = host;
+            this.username = username;
+            this.password = password;
+            this.port = port;
+            this.remotePath = remotePath;
+        }
+
+        /// <summary>
+        /// Make a request given a method and a file path
+        /// </summary>
+        /// <param name="file">The relative file path</param>
+        /// <param name="method">The ftp method i.e. what to do - <see cref="WebRequestMethods.Ftp"/></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        private bool MakeFTPRequest(string file, string method, Action<FtpWebRequest> callback = null)
+        {
+            Uri uri = new Uri($"ftp://{host}:{port}/{file}");
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(uri);
+            request.Method = method;
+            request.Credentials = new NetworkCredential(username, password);
+
+            callback?.Invoke(request);
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            response.Close();
+            return true;
+        }
+
+        /// <summary>
+        /// Delete a file from the server
+        /// </summary>
+        /// <param name="file">The relative file path</param>
+        public void Delete(string file)
+        {
+            try
+            {
+                MakeFTPRequest(file, WebRequestMethods.Ftp.DeleteFile);
+                MakeFTPRequest(file, WebRequestMethods.Ftp.RemoveDirectory);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        /// <summary>
+        /// renames a file on the server
+        /// </summary>
+        /// <param name="file">Relative file path</param>
+        /// <param name="newName">New name to rename to</param>
+        public void Rename(string file, string newName)
+        {
+            try
+            {
+                MakeFTPRequest(file, WebRequestMethods.Ftp.Rename, req => req.RenameTo = newName);
+            } catch (Exception ex)
+            {
+
+            }
+            
+        }
+
+        /// <summary>
+        /// Upload a file to the server
+        /// </summary>
+        /// <param name="fileLocal">Full local path</param>
+        /// <param name="fileServer">Relative server path</param>
+        public void Upload(string fileLocal, string fileServer)
+        {
+            Thread.Sleep(200);
+            try
+            {
+                if (Directory.Exists(fileLocal))
+                {
+                    MakeFTPRequest(fileServer, WebRequestMethods.Ftp.MakeDirectory);
+                } else
+                {
+                    MakeFTPRequest(fileServer, WebRequestMethods.Ftp.UploadFile, req =>
+                    {
+                        using (FileStream fs = File.OpenRead(fileLocal))
+                        {
+                            byte[] buffer = new byte[fs.Length];
+                            fs.Read(buffer, 0, buffer.Length);
+                            fs.Close();
+                            Stream requestStream = req.GetRequestStream();
+                            requestStream.Write(buffer, 0, buffer.Length);
+                            requestStream.Flush();
+                            requestStream.Close();
+                        }
+                    });
+                }
+                
+            } catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ FTPSync/FTPSync/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace FTPSync
+{
+    class Program
+    {
+        private static FTP ftp;
+        private static Uri filePath;
+        static void Main(string[] args)
+        {
+            string directory = Properties.Settings.Default.localPath;
+            string remotePath = Properties.Settings.Default.localPath;
+            string host = Properties.Settings.Default.localPath;
+            string port = Properties.Settings.Default.localPath;
+            string username = Properties.Settings.Default.localPath;
+            string password = Properties.Settings.Default.localPath;
+            ftp = new FTP(host, username, password, port, remotePath);
+            filePath = new Uri(directory);
+            Thread taskThd = new Thread(() => {
+                FileSystemWatcher fsw = new FileSystemWatcher(directory);
+                fsw.Filter = "";
+                fsw.IncludeSubdirectories = true;
+                fsw.EnableRaisingEvents = true;
+                fsw.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.DirectoryName;
+                fsw.Changed += FileChanged;
+                fsw.Created += FileCreated;
+                fsw.Deleted += FileDeleted;
+                fsw.Renamed += FileRenamed;
+            });
+
+            taskThd.Start();
+            while (true) ;
+        }
+
+        private static void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            string newName = Path.GetFileName(e.Name);
+            ftp.Rename(e.OldName, newName);
+        }
+
+        private static void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            ftp.Delete(e.Name);
+        }
+
+        private static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            ftp.Upload(e.FullPath, e.Name);
+        }
+
+        private static void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            ftp.Upload(e.FullPath, e.Name);
+        }
+    }
+}
